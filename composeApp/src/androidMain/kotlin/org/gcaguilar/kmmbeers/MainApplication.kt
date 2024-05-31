2c630b0382b737bd4ecd3f552c85ff93ffb0d204
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2024-05-31 13:27:14 +0200

    Remove wildcards Add EOF

--- composeApp/src/androidMain/kotlin/org/gcaguilar/kmmbeers/MainApplication.kt
+++ composeApp/src/androidMain/kotlin/org/gcaguilar/kmmbeers/MainApplication.kt
@@ -2,7 +2,6 @@ package org.gcaguilar.kmmbeers
 
 import android.app.Application
 import di.initKoin
-import org.koin.core.context.startKoin
 
 class MainApplication : Application() {
     override fun onCreate() {
--- composeApp/src/commonMain/kotlin/presentation/search/SearchScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/search/SearchScreen.kt
@@ -1,11 +1,16 @@
 package presentation.search
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp

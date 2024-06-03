8f7769148f049b567d041f07ba27aa8828af9970
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2024-06-03 17:07:14 +0200

    Log errors

--- composeApp/src/commonMain/kotlin/data/AuthenticationRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/data/AuthenticationRepositoryImpl.kt
@@ -7,14 +7,14 @@ class AuthenticationRepositoryImpl(
     private val secureStorage: SecureStorage
 ) : AuthenticationRepository {
     override suspend fun getLoginUrl(): Result<String> {
-        return authenticationService.getAuthenticationUrl().map {
+        return authenticationService.getAuthenticationUrl().mapCatching {
             it.loginUrl
         }
     }
 
     override suspend fun authenticate(code: String): Result<Unit> {
         return authenticationService.authorize(AuthenticationCode(code))
-            .map { secureStorage.setAccessToken(it.access_token) }
+            .mapCatching { secureStorage.setAccessToken(it.access_token) }
     }
 
     override fun isLoggedIn(): Boolean {

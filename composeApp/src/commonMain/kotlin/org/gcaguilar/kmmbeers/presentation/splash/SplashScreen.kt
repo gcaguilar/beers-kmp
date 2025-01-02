6b49c772f5b10924a31b4893e4fe501cc61bc194
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2025-01-02 12:36:06 +0100

    Update Splash logic navigation

--- composeApp/src/commonMain/kotlin/org/gcaguilar/kmmbeers/presentation/splash/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/org/gcaguilar/kmmbeers/presentation/splash/SplashScreen.kt
@@ -8,33 +8,36 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import kmmbeers.composeapp.generated.resources.Res
 import kmmbeers.composeapp.generated.resources.splash
+import org.gcaguilar.kmmbeers.presentation.Screen
 import org.jetbrains.compose.resources.vectorResource
 import org.koin.compose.koinInject
 import org.gcaguilar.kmmbeers.presentation.authentication.LoginScreen
 import org.gcaguilar.kmmbeers.presentation.search.SearchScreen
 import org.koin.compose.viewmodel.koinViewModel
+import co.touchlab.kermit.Logger
 
 private const val IsLoggedKey = "IsLoggedKey"
 
 @Composable
-fun SplashContent(
+fun Splash(
+    viewModel: SplashViewModel = koinViewModel<SplashViewModel>(),
+    navigate: (String) -> Unit,
 ) {
-    val viewModel = koinViewModel<SplashScreenModel>()
     val state = viewModel.state.collectAsState().value
 
     Image(
         modifier = Modifier.fillMaxSize(),
         imageVector = vectorResource(Res.drawable.splash),
-        contentDescription = "Splash Screen",
+        contentDescription = "Splash org.gcaguilar.kmmbeers.Screen",
     )
 
     state.isLoggedIn?.let { isLoggedIn ->
         val destination = if (isLoggedIn) {
-            //SearchScreen
+            Screen.Search.route
         } else {
-           // LoginScreen
+            Screen.Authentitcation.route
         }
-        // navigator.replace(destination)
+       navigate(destination)
     }
 
     LaunchedEffect(IsLoggedKey) {
--- composeApp/src/commonMain/kotlin/org/gcaguilar/kmmbeers/presentation/splash/SplashViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/gcaguilar/kmmbeers/presentation/splash/SplashViewModel.kt
@@ -1,25 +1,32 @@
 package org.gcaguilar.kmmbeers.presentation.splash
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import org.gcaguilar.kmmbeers.domain.IsLoggedIn
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import co.touchlab.kermit.Logger
 
 
-class SplashScreenModel(
-    //private val isLoggedIn: IsLoggedIn
+class SplashViewModel(
+    private val isLoggedIn: IsLoggedIn
 ) : ViewModel() {
     private val _state = MutableStateFlow(SplashState())
     val state: StateFlow<SplashState> = _state
+
     data class SplashState(
         val isLoggedIn: Boolean? = null
     )
 
     fun isLogged() {
-        val loggedIn = false
-        _state.update { state ->
-            state.copy(isLoggedIn = loggedIn)
+        viewModelScope.launch {
+            val loggedIn = isLoggedIn()
+            Logger.d(loggedIn.toString())
+            _state.update { state ->
+                state.copy(isLoggedIn = loggedIn)
+            }
         }
     }
 }

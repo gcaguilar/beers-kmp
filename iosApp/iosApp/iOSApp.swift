ef2dc0f9aa453f58f5a588896188822558704124
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2025-01-02 12:31:02 +0100

    Add dependencies for iOS

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,41 +1,17 @@
-import FirebaseCore
-import ComposeApp
 import SwiftUI
-import GoogleSignIn
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-
-    func application(
-      _ app: UIApplication,
-      open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-    ) -> Bool {
-      var handled: Bool
-
-      handled = GIDSignIn.sharedInstance.handle(url)
-      if handled {
-        return true
-      }
-
-      // Handle other custom URL types.
-
-      // If not handled by this app, return false.
-      return false
-    }
-
-
-}
+import ComposeApp
+import Firebase
 
 @main
 struct iOSApp: App {
-	init() {
+    init() {
         HelperKt.doInitKoin()
         FirebaseApp.configure()
     }
-   var body: some Scene {
-      WindowGroup {
-            ContentView().onOpenURL(perform: { url in
-                GIDSignIn.sharedInstance.handle(url)
-            })
-      }
-   }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
 }
